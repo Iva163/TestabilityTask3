199f0cdb1cd5a203eaa016720b3a51ee61e345c7
Ivan Borisov <ivan_borisov_88@vk.com>
2023-01-04 21:59:58 +0400

    init

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,8 @@
+public class CreditPaymentService {
+    public int calculate (double p, int s, int t) {
+        double k = s*((((p/12)/100)*Math.pow(1+((p/12)/100),(t*12)))/(Math.pow(1+((p/12)/100),(t*12)) - 1));
+        int b = (int) k;
+        return b;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService paymentService = new CreditPaymentService();
+        System.out.println("Ежемесячный платеж при сумме кредита 1_000_000, сроке 1 год, ставке 9,99%: " + paymentService.calculate(9.99, 1_000_000, 1) + " руб");
+        System.out.println("Ежемесячный платеж при сумме кредита 1_000_000, сроке 2 года, ставке 9,99%: " + paymentService.calculate(9.99, 1_000_000, 2) + " руб");
+        System.out.println("Ежемесячный платеж при сумме кредита 1_000_000, сроке 3 года, ставке 9,99%: " + paymentService.calculate(9.99, 1_000_000, 3) + " руб");
+        }
+    }
